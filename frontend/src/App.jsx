6c276c48a3b9e5d6e4c42fce2bcc3627668a07b0
hsingla378 <37644800+hsingla378@users.noreply.github.com>
2024-01-03 08:54:02 +0530

    add nav routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
 import Navbar from "./components/navbar.component";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <Navbar />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route path="signin" element={<h1>Sign in page</h1>} />
+        <Route path="signup" element={<h1>Sign up page</h1>} />{" "}
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
